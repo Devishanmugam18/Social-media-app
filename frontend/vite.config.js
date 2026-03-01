d9b4b9e70ac24498a11e039463816e9253558377
Devi Shanmugam <you@example.com>
2026-03-01 20:28:17 +0530

    vite config host added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     allowedHosts: ["nondomineering-dot-untimidly.ngrok-free.dev"],
+    host: "0.0.0.0",
   },
 });
