c67f8acfba25ab87030d2d6c79b364796e87e526
Devi Shanmugam <you@example.com>
2026-03-01 10:14:51 +0530

    uncomment

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Post = require("../ORMmodels/Post");
+// const Post = require("../ORMmodels/Post");
 const jwt = require("jsonwebtoken");
 const myPosts = require("../ORMmodels/myPosts");
 const feedPosts = require("../ORMmodels/feed");
