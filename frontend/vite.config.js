458fcd7a8916788a870702684c517868f744f474
Devi Shanmugam <you@example.com>
2026-03-02 10:26:40 +0530

    added aws domain as allowed host in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   plugins: [react()],
 
   server: {
-    allowedHosts: ["nondomineering-dot-untimidly.ngrok-free.dev"],
+    allowedHosts: [
+      "nondomineering-dot-untimidly.ngrok-free.dev",
+      "ec2-13-235-246-80.ap-south-1.compute.amazonaws.com",
+    ],
     host: "0.0.0.0",
   },
 });
