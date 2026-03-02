ea9ba7ae7069c99d955d8af604084e33be33c895
Devi Shanmugam <you@example.com>
2026-03-02 11:02:06 +0530

    allowed host:all in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,11 @@ export default defineConfig({
   plugins: [react()],
 
   server: {
-    allowedHosts: [
-      "nondomineering-dot-untimidly.ngrok-free.dev",
-      "ec2-13-235-246-80.ap-south-1.compute.amazonaws.com",
-    ],
+    // allowedHosts: [
+    //   "nondomineering-dot-untimidly.ngrok-free.dev",
+    //   "ec2-13-235-246-80.ap-south-1.compute.amazonaws.com",
+    // ],
+    allowedHosts: "all",
     host: "0.0.0.0",
   },
 });
